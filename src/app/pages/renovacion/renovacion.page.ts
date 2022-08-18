9649637b35e6ea049a1ab70f46d0d2f9fcea72ce
RamonRojas9987 <72566506+RamonRojas9987@users.noreply.github.com>
2022-08-17 22:53:42 -0500

    Menus
    Se agregan menus condicionales

--- src/app/pages/renovacion/renovacion.page.ts
+++ src/app/pages/renovacion/renovacion.page.ts
@@ -65,4 +65,265 @@ export class RenovacionPage implements OnInit {
     loading.present();
   }
 
+  private tutorialHidden: boolean = true;
+
+  abrirTutorial(){
+
+    if(this.tutorialHidden === true){
+
+      this.tutorialHidden = false;
+      document.getElementById("tutorial").hidden = false;
+
+    }else if(this.tutorialHidden === false){
+
+      this.tutorialHidden = true;
+      document.getElementById("tutorial").hidden = true;
+
+    }
+
+  }
+
+  private centroHidden: boolean = true;
+
+  abrirCentro1(){
+
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("AO").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("AO").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro2(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("AZ").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("AZ").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro3(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("BJ").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("BJ").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro4(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("CY").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("CY").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro5(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("CJ").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("CJ").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro6(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("CH").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("CH").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro7(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("GAM").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("GAM").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro8(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("IZ").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("IZ").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro9(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("IZT").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("IZT").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro10(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("MC").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("MC").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro11(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("MH").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("MH").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro12(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("MA").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("MA").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro13(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("TL").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("TL").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro14(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("VC").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("VC").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro15(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("XC").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("XC").hidden = true;
+
+    }
+
+  }
+
 }
+
