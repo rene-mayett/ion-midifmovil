f8d1feb43042505364685baf3fc9da2b33249c94
René Mayett <44554119+rene-mayett@users.noreply.github.com>
2022-08-18 12:37:56 -0500

    18/08/2022 12:40 p m
    ctm github

--- src/app/pages/renovacion/renovacion.page.ts
+++ src/app/pages/renovacion/renovacion.page.ts
@@ -15,337 +15,307 @@ export class RenovacionPage implements OnInit {
 
   curp = sessionStorage.getItem('curp');
   usuarios;
-  tutor;
+  tutor= sessionStorage.getItem('tutor');
+  dir1;
+  dir2;
   ngOnInit() {
-    console.log(sessionStorage.getItem('curp'));
-    // this.usuarios = this.APIService.perfilLeona(this.curp);
-    // console.log(this.usuarios);
+    console.log(this.tutor)
     this.cargando();
     this.APIService.perfilReno(this.curp).subscribe(res => {
         this.usuarios = res;
-        console.log(this.usuarios);
-        this.tutor = this.usuarios[0].nombre_tutor+' '+this.usuarios[0].apellido_paterno_tutor + ' ' + this.usuarios[0].apellido_materno_tutor;
+        console.log(this.usuarios,'DIRECCION AQUI');
+        this.dir1 = this.usuarios[0].nombre_vialidad+', '+this.usuarios[0].num_exterior+', int. '+this.usuarios[0].num_interior;
+        this.dir2 = this.usuarios[0].nombre_asentamiento+', '+this.usuarios[0].delegacion+', CP '+this.usuarios[0].codigo_postal;
     },
     (err) => {
       console.log(err);
-      this.router.navigate(['leona'],);
-      sessionStorage.clear();
       this.errorAlert();
     });
-    
-    this.cargando();
     this.APIService.perfilTutor(this.curp).subscribe(res => {
-        this.usuarios = res;
-        console.log(this.usuarios);
-        this.tutor = this.usuarios[0].nombre_tutor+' '+this.usuarios[0].apellido_paterno_tutor + ' ' + this.usuarios[0].apellido_materno_tutor;
-        sessionStorage.setItem('tutor', this.tutor);
+      this.usuarios = res;
     },
     (err) => {
       console.log(err);
-      this.router.navigate(['leona'],);
-      sessionStorage.clear();
-      this.errorAlert();
     });
-
-
-  }
-
-  async errorAlert() {
-    const alert = await this.alertController.create({
-      header: 'Error 503',
-      subHeader: 'Error durante la conexión ',
-      message: 'No se puede conectar con el servidor, por favor vuelva a iniciar sesión',
-      buttons: ['OK'],
-    });
-    await alert.present();
   }
 
   async cargando() {
     const loading = await this.loadingCtrl.create({
       message: 'Cargando...',
-      duration: 1000,
+      duration: 800,
     });
     loading.present();
+    }
+
+    async errorAlert() {
+      const alert = await this.alertController.create({
+        header: 'Error 503',
+        subHeader: 'Error durante la conexión ',
+        message: 'No se puede conectar con el servidor',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
+
+    private tutorialHidden: boolean = true;
+
+  abrirTutorial(){
+
+    if(this.tutorialHidden === true){
+
+      this.tutorialHidden = false;
+      document.getElementById("tutorial").hidden = false;
+
+    }else if(this.tutorialHidden === false){
+
+      this.tutorialHidden = true;
+      document.getElementById("tutorial").hidden = true;
+
+    }
+
   }
 
-}
-// var direccionesAOB = new Array();
-// var direccionesAZC = new Array();
-// var direccionesBJ = new Array();
-// var direccionesCY = new Array();
-// var direccionesCJ = new Array();
-// var direccionesCUA = new Array();
-// var direccionesGAM = new Array();
-// var direccionesIZT = new Array();
-// var direccionesIZTP = new Array();
-// var direccionesMGC = new Array();
-// var direccionesMAL = new Array();
-// var direccionesTLA = new Array();
-// var direccionesVCA = new Array();
-// var direccionesXCM = new Array();
+  private centroHidden: boolean = true;
+
+  abrirCentro1(){
+
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("AO").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("AO").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro2(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("AZ").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("AZ").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro3(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("BJ").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("BJ").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro4(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("CY").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("CY").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro5(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("CJ").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("CJ").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro6(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("CH").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("CH").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro7(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("GAM").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("GAM").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro8(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("IZ").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("IZ").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro9(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("IZT").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("IZT").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro10(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("MC").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("MC").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro11(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("MH").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("MH").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro12(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("MA").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("MA").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro13(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("TL").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("TL").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro14(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("VC").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("VC").hidden = true;
+
+    }
+
+  }
+
+  abrirCentro15(){
+    
+    if(this.centroHidden === true){
+
+      this.centroHidden = false;
+      document.getElementById("XC").hidden = false;
+
+    }else if(this.centroHidden === false){
+
+      this.centroHidden = true;
+      document.getElementById("XC").hidden = true;
+
+    }
+
+  }
 
 
-// function direccion(form) {
-//       var selec = form.tipos.options;
-//       var combo = form.estilo.options;
-//       if (selec[1].selected == true) {
-//           document.forms.item = direccionesAOB[combo.selectedIndex];
-//       }
-//       if (selec[2].selected == true) {
-//   //         form.action = direccionesAZC[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[3].selected == true) {
-//   //         form.action = direccionesBJ[combo.selectedIndex];
-//   //     }
-//   //     if (selec[4].selected == true) {
-//   //         form.action = direccionesCY[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[5].selected == true) {
-//   //         form.action = direccionesCJ[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[6].selected == true) {
-//   //         form.action = direccionesCUA[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[7].selected == true) {
-//   //         form.action = direccionesGAM[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[8].selected == true) {
-//   //         form.action = direccionesIZT[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[9].selected == true) {
-//   //         form.action = direccionesIZTP[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[10].selected == true) {
-//   //         form.action = direccionesMGC[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[11].selected == true) {
-//   //         form.action = direccionesMAL[combo.selectedIndex];
-//   //     }
-//   //     if (selec[12].selected == true) {
-//   //         form.action = direccionesTLA[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[13].selected == true) {
-//   //         form.action = direccionesVCA[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[14].selected == true) {
-//   //         form.action = direccionesXCM[combo.selectedIndex];
-//   //     }
-  
-//   //     if (selec[15].selected == true) {
-//   //         form.action = direccionesBJ[combo.selectedIndex];
-//   //     }
-  
-  
-  
-  
-//   //     /*Se puede quitar una vez vemos que funciona*/
-  
-//   // }
-  
-  
-  
-//   // function agregarOpciones(form) {
-//   //     var selec = form.tipos.options;
-//   //     var combo = form.estilo.options;
-//   //     combo.length = null;
-  
-//   //     if (selec[0].selected == true) {
-//   //         var seleccionar = new Option("Esperando selección");
-//   //         combo[0] = seleccionar;
-//   //     }
-  
-//   //     if (selec[1].selected == true) {
-//   //         var centro0 = new Option("Área Operativa Poniente");
-//   //         var centro1 = new Option("C.A.D.I. 17");
-//   //         var centro2 = new Option("C.D.C. Jalalpa");
-//   //         var centro3 = new Option("C.F. 18 Leandro Valle");
-//   //         var centro4 = new Option("C.F. 24 León Guzman");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //         combo[4] = centro4;
-//   //     }
-  
-//   //     if (selec[2].selected == true) {
-//   //         var centro0 = new Option("C.B.S.U. San Juan Tlihuaca");
-//   //         var centro1 = new Option("C.D.C. Pacto Federal");
-//   //         var centro2 = new Option("C.F. 01 Jose María Mata");
-//   //         var centro3 = new Option("C.F. 03 Miguel Lerdo de Tejada");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //     }
-  
-//   //     if (selec[3].selected == true) {
-//   //         var centro0 = new Option("Centro Recreativo Niños Héroes");
-//   //         combo[0] = centro0;
-//   //     }
-//   //     if (selec[4].selected == true) {
-//   //         var centro0 = new Option("C.B.S.U. Ajusco");
-//   //         var centro1 = new Option("C.D.C. Adolfo Ruiz Cortines");
-//   //         var centro2 = new Option("C.D.C. Familia Juárez Maza");
-//   //         var centro3 = new Option("C.D.C. Francisco I. Madero");
-//   //         var centro4 = new Option("C.F. 17 Jose María Díaz Ordaz");
-//   //         var centro5 = new Option("C.F. 20 Gral. Manuel González Ortega");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //         combo[4] = centro4;
-//   //         combo[5] = centro5;
-//   //     }
-  
-//   //     if (selec[5].selected == true) {
-//   //         var centro0 = new Option("C.D.C. Jose María Morelos y Pavón");
-//   //         combo[0] = centro0;
-//   //     }
-  
-//   //     if (selec[6].selected == true) {
-//   //         var centro0 = new Option("C.A.D.I. 15");
-//   //         var centro1 = new Option("C.A.D.I. 19");
-//   //         var centro2 = new Option("C.A.D.I. 20");
-//   //         var centro3 = new Option("C.D.C. Cuauhtémoc");
-//   //         var centro4 = new Option("C.D.C. República Española");
-//   //         var centro5 = new Option("Centro de día Núm. 1");
-//   //         var centro6 = new Option("Centro de día Núm. 2");
-//   //         var centro7 = new Option("Comedor Familiar Núm. 1");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //         combo[4] = centro4;
-//   //         combo[5] = centro5;
-//   //         combo[6] = centro6;
-//   //         combo[7] = centro7;
-//   //     }
-  
-//   //     if (selec[7].selected == true) {
-//   //         var centro0 = new Option("Área Operativa Norte");
-//   //         var centro1 = new Option("C.B.S.U. Cuautepec");
-//   //         var centro2 = new Option("C.B.S.U. Nueva Atzacoalco");
-//   //         var centro3 = new Option("C.B.S.U. Zacatenco");
-//   //         var centro4 = new Option("C.D.C. Álvaro Obregón");
-//   //         var centro5 = new Option("C.D.C. Lázaro Cárdenas");
-//   //         var centro6 = new Option("C.F. 08 Niño Jose Luis Ordaz Lopez");
-//   //         var centro7 = new Option("C.F. 09 Francisco Zarco");
-//   //         var centro8 = new Option("C.F. 10 Santos Degollado");
-//   //         var centro9 = new Option("C.F. 11 Miguel Ramos Arizpe");
-//   //         var centro10 = new Option("C.F. 12 Dr. Jose María Luis Mora");
-//   //         var centro11 = new Option("C.F. 13 Matías Romero");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //         combo[4] = centro4;
-//   //         combo[5] = centro5;
-//   //         combo[6] = centro6;
-//   //         combo[7] = centro7;
-//   //         combo[8] = centro8;
-//   //         combo[9] = centro9;
-//   //         combo[10] = centro10;
-//   //         combo[11] = centro11;
-//   //     }
-  
-//   //     if (selec[8].selected == true) {
-//   //         var centro0 = new Option("C.B.S.U. Benito Juárez");
-//   //         var centro1 = new Option("C.D.C. Felipe Carrillo Puerto");
-//   //         var centro2 = new Option("C.D.C. Ricardo Flores Magón");
-//   //         var centro3 = new Option("C.D.C. Santa Anita");
-//   //         var centro4 = new Option("C.F. 14 Gral. Juan N. Álvarez");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //         combo[4] = centro4;
-//   //     }
-  
-//   //     if (selec[9].selected == true) {
-//   //         var centro0 = new Option("Albergue Central de Abastos");
-//   //         var centro1 = new Option("Área Operativa Oriente");
-//   //         var centro2 = new Option("C.B.S.U. Juan Escutia");
-//   //         var centro3 = new Option("C.D.C. Constitución de 1917");
-//   //         var centro4 = new Option("C.D.C. Ignacio Zaragoza");
-//   //         var centro5 = new Option("C.D.C. Iztapalapa Nuevo");
-//   //         var centro6 = new Option("C.D.C. Josefa Ortiz de Domínguez");
-//   //         var centro7 = new Option("C.D.C. Vicente Guerrero");
-//   //         var centro8 = new Option("C.E.C.A.P.I.T.");
-//   //         var centro9 = new Option("C.F. 06 Ignacio Manuel Altamirano");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //         combo[4] = centro4;
-//   //         combo[5] = centro5;
-//   //         combo[6] = centro6;
-//   //         combo[7] = centro7;
-//   //         combo[8] = centro8;
-//   //         combo[9] = centro9;
-//   //     }
-  
-//   //     if (selec[10].selected == true) {
-//   //         var centro0 = new Option("C.B.S.U. Argentina");
-//   //         var centro1 = new Option("C.D.C. XLIX Legislatura");
-//   //         var centro2 = new Option("C.F. 07 Melchor Ocampo");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //     }
-  
-//   //     if (selec[11].selected == true) {
-//   //         var centro0 = new Option("C.D.C. Milpa Alta");
-//   //         combo[0] = centro0;
-//   //     }
-  
-//   //     if (selec[12].selected == true) {
-//   //         var centro0 = new Option("C.D.C. Quetzalcóatl");
-//   //         var centro0 = new Option("C.D.C. Tláhuac");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-  
-//   //     }
-  
-//   //     if (selec[13].selected == true) {
-//   //         var centro0 = new Option("Área Operativa Sur");
-//   //         var centro1 = new Option("C.D.C. Ajusco Tlalpan");
-//   //         var centro2 = new Option("C.D.C. Miguel Hidalgo");
-//   //         var centro3 = new Option("C.F. 19 Juan A. Mateos");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //     }
-  
-//   //     if (selec[14].selected == true) {
-//   //         var centro0 = new Option("C.A.D.I. 31");
-//   //         var centro1 = new Option("C.D.C. Adolfo López Mateos");
-//   //         var centro2 = new Option("C.D.C. Rosario Castellanos");
-//   //         var centro3 = new Option("C.D.C. Venustiano Carranza");
-//   //         var centro4 = new Option("C.F. 02 General Ignacio Zaragoza");
-//   //         var centro5 = new Option("C.F. 04 Ignacio Ramirez");
-//   //         combo[0] = centro0;
-//   //         combo[1] = centro1;
-//   //         combo[2] = centro2;
-//   //         combo[3] = centro3;
-//   //         combo[4] = centro4;
-//   //         combo[5] = centro5;
-//   //     }
-  
-//   //     if (selec[15].selected == true) {
-//   //         var centro0 = new Option("C.D.C. Xochimilco");
-//   //         combo[0] = centro0;
-//   //     }
-//   // }
-  
-
+}   
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -13,17 +13,24 @@ export class APIService {
   loginLeona(usuario): Observable<any> {
     return this.http.get(`${environment.APIurl}/login?curp=${usuario.curp}&idp=${usuario.idp}`);
   }
-
   perfilLeona(curp): Observable<any> {
-    return this.http.get(`${environment.APIurl}/menu?curp=${curp}`);
+    return this.http.get(`${environment.APIurl}/menu?curp=${curp}`); // RENE
   }
 
+  validaActivosRenovacion(curp): Observable<any> {
+    return this.http.get(`${environment.APIurl}/idbaja?curp=${curp}`); // RENE
+  }
+
+  // yaRenovo(curp): Observable<any> {
+  //   return this.http.get(`${environment.APIurl}/menu?curp=${curp}`); // RENE
+  // }
+
   perfilReno(curp): Observable<any>{
-    return this.http.get(`${environment.APIurl}/reno?curp=${curp}`)
+    return this.http.get(`${environment.APIurl}/reno?curp=${curp}`) // RAMON
   }
 
   perfilTutor(curp): Observable<any>{
-    return this.http.get(`${environment.APIurl}/tutor?curp=${curp}`)
+    return this.http.get(`${environment.APIurl}/tutor?curp=${curp}`) // RAMON
   }
 
 }
